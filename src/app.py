11d0a6cf31af271678ff7f3f5a93c3afae144e75
Meagan Provencher <69221048+mrprov12@users.noreply.github.com>
2024-06-29 14:15:49 -0600

    feat: Add helper functions to initialize database schema and handle database operations
    - Defined init_db function in database.py to create all necessary tables
    - Added get_db_connection function to establish a database connection using environment variables
    - Implemented get_credentials function to fetch stream credentials for a given user and stream type
    - Created execute_query function to execute SQL queries and handle transactions
    - Updated environment variables to include database and stream credentials
    - Created test suite for db
    
    This setup ensures the database schema is properly initialized and provides utility functions for database operations in the ChattyPaws backend.

--- src/app.py
+++ src/app.py
@@ -1,5 +1,9 @@
-import os
 from dotenv import load_dotenv
+from repo.database import init_db
 
 # Load environment variables from .env file
 load_dotenv()
+
+if __name__ == "__main__":
+   # Init PostgreSQL chattypaws-backend-db 
+    init_db()
--- src/repo/database.py
+++ src/repo/database.py
@@ -2,14 +2,38 @@ import psycopg2
 import os
 from dotenv import load_dotenv
 
+from models.user import User
+from models.pet import Pet
+from models.button import Button
+from models.tile import Tile
+from models.button_press import ButtonPress
+from models.video_stream import VideoStream
+from models.session_history import SessionHistory
+from models.setting import Setting
+from models.notification import Notification
+from models.stream_credential import StreamCredential
+
 # Load environment variables from .env file
 load_dotenv()
+
+def init_db():
+    User.create_table()
+    Pet.create_table()
+    Button.create_table()
+    Tile.create_table()
+    ButtonPress.create_table()
+    VideoStream.create_table()
+    SessionHistory.create_table()
+    Setting.create_table()
+    Notification.create_table()
+    StreamCredential.create_table()
+    print("Database initialized successfully!")
 
 def get_db_connection():
     conn = psycopg2.connect(
         host=os.getenv("DB_HOST"),
         port=os.getenv("DB_PORT"),
-        database=os.getenv("DB_NAME"),
+        database=os.getenv("DB_DATABASE_NAME"),
         user=os.getenv("DB_USERNAME"),
         password=os.getenv("DB_PASSWORD")
     )
@@ -34,7 +58,9 @@ def execute_query(query, params=None):
         with conn.cursor() as cur:
             cur.execute(query, params)
             conn.commit()
-            return cur.fetchall()
+            if query.strip().lower().startswith('select'):
+                return cur.fetchall()
+            return None
     except Exception as e:
         conn.rollback()
         raise e
